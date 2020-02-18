267dff16f7aa157c5098cb3c3362fdebe8e3e20d
Bukva Dávid <davidbukva@gmail.com>
2020-02-18 22:34:39 +0100

    Deleted test cout s. Cleaned up a bit.

--- readmnist.cpp
+++ readmnist.cpp
@@ -10,15 +10,12 @@ using namespace std;
 int readmnistdata(int ***training_data, int ***test_data, int ***validation_data){
 	int fd;
 	char buf[5];
-	for(int i = 0; i < 5; i++){
-		((char *)(buf))[i]=0x00;
-	}
-	cout << "successful 1\n";
-	fd = open("mnist",O_RDONLY);
-	if(fd == -1)return -1;
-	cout << "successful 2 " << fd << "\n";
+
+
+	if((fd = open("mnist",O_RDONLY)) == -1)return -1;
+
 	if(read(fd,buf,5)==-1)return -1;
-	cout << "successful 3\n";
+
 	cout << buf << endl;
 
 	close(fd);
