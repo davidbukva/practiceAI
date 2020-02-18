ecfb54971bddd940a44dc9c87545df053ee6a8ca
Bukva Dávid <davidbukva@gmail.com>
2020-02-18 22:30:50 +0100

    Continued readmnist.cpp

--- readmnist.cpp
+++ readmnist.cpp
@@ -1,16 +1,28 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int readmnistdata(int ***training_data, int ***test_data, int ***validation_data){
 	int fd;
-	char *buf;
-
-	if(fd = open("mnist", O_RDONLY) == -1)return -1;
-	
+	char buf[5];
+	for(int i = 0; i < 5; i++){
+		((char *)(buf))[i]=0x00;
+	}
+	cout << "successful 1\n";
+	fd = open("mnist",O_RDONLY);
+	if(fd == -1)return -1;
+	cout << "successful 2 " << fd << "\n";
 	if(read(fd,buf,5)==-1)return -1;
+	cout << "successful 3\n";
+	cout << buf << endl;
+
+	close(fd);
+
 
 	return 0;
 	
