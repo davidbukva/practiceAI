c0b1bc6ba8a75ade4f6896038192e4acf3e60f6c
Bukva Dávid <davidbukva@gmail.com>
2020-02-20 21:17:24 +0100

    cont matrix.h

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include "matrix.h"
+#include <string>
 using namespace std;
 
-int main(){
+int main(int nargs,char **args){
+	int n,m;
+	if(nargs<3){
+		cerr << "Usage: " << args[0] << " " << "n m" << endl;
+		return -1;
+	}
+	n = stoi(args[1]);
+	m = stoi(args[2]);
+
+	matrix m1(n,m);
+
+	m1[0][0]=244;
 	
-	cout << "hello world\n";
+	matrix mat(m1);
+	mat.print();
+	
 
 }
---
+++ matrix.h
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <iostream>
+
+
+struct matrix{
+
+    int n;
+    int m;
+
+    int **data;
+
+    matrix(int _n, int _m): n(_n),m(_m){
+        data = (int **)malloc(m*sizeof(int *));
+		for(int i = 0; i < m; i++){
+			data[i] = (int *)malloc(n*sizeof(int));
+			for(int j = 0; j < n; j++){
+				data[i][j]=0;
+			}
+		}
+    }
+
+	matrix(const matrix &mat){
+		matrix(mat.n,mat.m,mat.data);
+	}
+
+	matrix(int _n, int _m, int **_data): n(_n), m(_m){
+		data = (int **)malloc(m*sizeof(int *));
+		for(int i = 0; i < m; i++){
+			data[i] = (int *)malloc(n*sizeof(int));
+			for(int j = 0; j < n; j++){
+				data[i][j]=_data[i][j];
+			}
+		}
+	}
+    
+	int *operator[](int pos){
+		return data[pos];
+	}
+
+	void print(){
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++){
+				std::cout << data[i][j] << " ";
+			}std::cout << "\n";
+		}
+	}
+
+	~matrix(){
+		free(data);
+	}
+
+
+
+};
